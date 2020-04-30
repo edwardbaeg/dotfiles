95c445bdf02f40b74f4edc539f945a6e62054344
edwardbaeg <edwardbaeg9@gmail.com>
2020-04-29 20:44:19 -0700

    Improve symscript

--- symlinks.sh
+++ symlinks.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-ln -s ~/dev/dotfiles/init.lua ~/.hammerspoon/init.lua
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+HAMMERSPOON_PATH="$HOME/.hammerspoon/init.lua"
+if test -f "$HAMMERSPOON_PATH"; then
+  # remove link
+  unlink $HAMMERSPOON_PATH
+
+  # create a backup
+  echo "hammerspoon file exists, creating backup"
+  mv $HAMMERSPOON_PATH $HAMMERSPOON_FILE.backup
+fi
+
+ln -s $DIR/init.lua $HAMMERSPOON_PATH
+echo "symlinked hammerspoon"
