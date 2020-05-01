978e143b1d74b1d48b819ee8b138cb12ad71b2e7
edwardbaeg <edwardbaeg9@gmail.com>
2020-04-30 23:54:09 -0700

    Add resizeAndCenter

--- init.lua
+++ init.lua
@@ -1,25 +1,27 @@
 -- ~/.hammerspoon/init.lua
 
+hyperkey = { "cmd", "ctrl" }
+
 -- Hammerspoon notification
-hs.hotkey.bind({"cmd", "ctrl"}, "W", function()
+hs.hotkey.bind(hyperkey, "W", function()
   hs.alert.show("Hello World!")
 end)
 
 -- Native notification
-hs.hotkey.bind({"cmd", "ctrl"}, "W", function()
+hs.hotkey.bind(hyperkey, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
 
 -- Hotkey to reload configuration
 -- NOTE: hs.reload() destroys current Lua interpreter so anything after it is
 --  ignored
-hs.hotkey.bind({"cmd", "ctrl"}, "R", function()
+hs.hotkey.bind(hyperkey, "R", function()
   hs.reload()
 end)
 hs.alert.show("Config loaded")
 
 -- Move window over
--- hs.hotkey.bind({"cmd", "ctrl"}, "B", function()
+-- hs.hotkey.bind(hyperkey, "B", function()
 --   local win = hs.window.focusedWindow()
 --   local f = win:frame()
 
@@ -28,7 +30,7 @@ hs.alert.show("Config loaded")
 -- end)
 
 -- Move window to right half
-hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
+hs.hotkey.bind(hyperkey, "L", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screenMax = win:screen():frame()
@@ -42,7 +44,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
 end)
 
 -- Move window to left half
-hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
+hs.hotkey.bind(hyperkey, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screenMax = win:screen():frame()
@@ -56,7 +58,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
 end)
 
 -- Move window to top half
-hs.hotkey.bind({"cmd", "ctrl"}, "K", function()
+hs.hotkey.bind(hyperkey, "K", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screenMax = win:screen():frame()
@@ -70,7 +72,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "K", function()
 end)
 
 -- Move window to bottom half
-hs.hotkey.bind({"cmd", "ctrl"}, "J", function()
+hs.hotkey.bind(hyperkey, "J", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screenMax = win:screen():frame()
@@ -82,6 +84,39 @@ hs.hotkey.bind({"cmd", "ctrl"}, "J", function()
   f.h = screenMax.h /2
   win:setFrame(f, needsResize and 0 or 0.1)
 end)
+
+function resizeAndCenter(frac)
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screenMax = win:screen():frame()
+
+  f.w = screenMax.w * frac
+  f.h = screenMax.h * frac
+  f.x = screenMax.x + (screenMax.w - f.w) / 2
+  f.y = screenMax.y + (screenMax.h - f.h) / 2
+  win:setFrame(f, 0)
+end
+
+-- Maximize window
+hs.hotkey.bind(hyperkey, "M", function()
+  resizeAndCenter(0.98)
+end)
+
+-- Resize window
+hs.hotkey.bind(hyperkey, "N", function()
+  resizeAndCenter(0.81)
+end)
+
+-- Resize window
+hs.hotkey.bind(hyperkey, "B", function()
+  resizeAndCenter(0.64)
+end)
+
+-- Resize window
+hs.hotkey.bind(hyperkey, "V", function()
+  resizeAndCenter(0.49)
+end)
+
 
 --[[ NOTES
 
