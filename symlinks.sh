e27fe70b4cd75ad6089e826d7d974995e23bf20c
edwardbaeg <edwardbaeg9@gmail.com>
2020-05-03 00:03:24 -0700

    Move to directories

--- symlinks.sh
+++ symlinks.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Get the dir of this file (NOT necessarily the current working dir)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 HAMMERSPOON_PATH="$HOME/.hammerspoon/init.lua"
-if test -f "$HAMMERSPOON_PATH"; then
+if test -L "$HAMMERSPOON_PATH"; then
   # remove link
   unlink $HAMMERSPOON_PATH
-
-  # create a backup
+elif test -f "$INITVIM_PATH"; then
+  # delete and create a backup
   echo "hammerspoon file exists, creating backup"
   mv $HAMMERSPOON_PATH $HAMMERSPOON_PATH.backup
 fi
 
-ln -s $DIR/init.lua $HAMMERSPOON_PATH
+mkdir -p $HOME/.hammerspoon
+ln -s $DIR/hammerspoon/init.lua $HAMMERSPOON_PATH
 echo "symlinked hammerspoon"
+
+INITVIM_PATH="$HOME/.config/nvim/init.vim"
+# Checks if symbolic link (but file may not exist)
+if test -L "$INITVIM_PATH"; then
+  # Remove link first
+  unlink $INITVIM_PATH
+# Checks if file exists
+elif test -f "$INITVIM_PATH"; then
+  # delete and create a backup
+  echo "init.vim file exists, creating backup"
+  mv $INITVIM_PATH $INITVIM_PATH.backup
+fi
+
+mkdir -p $HOME/.config/nvim/
+ln -s $DIR/vim/init.vim $INITVIM_PATH
+echo "symlinked init.vim"
