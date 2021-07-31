333929a4d23d567c8312b152a645cd4d53dc793e
edwardbaeg <edwardbaeg9@gmail.com>
2021-07-31 12:53:34 -0700

    Thicker highlight borders

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -33,7 +33,7 @@ end)
 --------------------------------------------------------------------------
 hs.window.highlight.ui.overlay = false
 hs.window.highlight.ui.overlayColor = {0,0,0,0.01} -- overlay color
-hs.window.highlight.ui.frameWidth = 6 -- draw a frame around the focused window in overlay mode; 0 to disable
+hs.window.highlight.ui.frameWidth = 8 -- draw a frame around the focused window in overlay mode; 0 to disable
 hs.window.highlight.start()
 
 -- Toggle on fullscreen toggle
