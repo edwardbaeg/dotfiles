7d86d5d042ea3d203ebb4b118675a12c30d04b10
edwardbaeg <edwardbaeg9@gmail.com>
2022-01-13 00:08:22 -0500

    Update highlight styling

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -33,7 +33,7 @@ end)
 --------------------------------------------------------------------------
 hs.window.highlight.ui.overlayColor = {0,0,0,0.01} -- overlay color
 hs.window.highlight.ui.overlay = false
-hs.window.highlight.ui.frameWidth = 8 -- draw a frame around the focused window in overlay mode; 0 to disable
+hs.window.highlight.ui.frameWidth = 4 -- draw a frame around the focused window in overlay mode; 0 to disable
 hs.window.highlight.start()
 
 -- Toggle on fullscreen toggle
