8416ff6fabd46f11819ccff813b0a30025bbcefd
edwardbaeg <edwardbaeg9@gmail.com>
2023-08-04 19:41:49 -0500

    Refactor to lua and organize

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,23 +1,37 @@
 -- [[ Keymaps ]]
 vim.keymap.set("i", "jk", "<Esc>") -- leave insert mode
+vim.keymap.set("n", "_", '"_') -- empty register shortcut
+vim.keymap.set("n", "H", "^") -- go to start of line
+vim.keymap.set("n", "L", "$") -- go to start of line
+vim.keymap.set("n", "Y", "y$") -- yank to end of line (like C or D)
+vim.keymap.set("n", "gp", "`[v`]") -- visually select previouly selected text
+-- vim.keymap.set("n", "p", "p`[v`]=") -- indent after pasting -- this breaks yanky
+vim.keymap.set("n", "<c-f>", "za") -- toggle folds
 
+-- leader keymaps
 vim.keymap.set("n", "<leader>+", "<c-a>") -- increment and decrement
 vim.keymap.set("n", "<leader>-", "<c-x>")
 vim.keymap.set("n", "<leader>ex", ":ex .<cr>", { desc = "open netrw in directory :ex ." }) -- open netrw
-vim.keymap.set("n", "_", '"_') -- empty register shortcut
 vim.keymap.set("n", "<leader>q", "") -- close whichkey / cancel leader without starting macro
+vim.keymap.set("n", "<leader><space>", ":nohlsearch<Bar>:echo<cr>", { desc = "clear searches" })
+vim.keymap.set("n", "<leader>yy", "ggyG''") -- yank whole file
+vim.keymap.set("n", "<leader>o", "i<cr><esc>") -- split line
 
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true }) -- Remaps for dealing with word wrap
+-- Remaps for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set("n", "<leader>yy", "ggyG''") -- yank whole file
-
--- vim.keymap.set('n', 'gE', vim.diagnostic.goto_prev) -- Diagnostic keymaps
+-- Diagnostic keymaps
+-- vim.keymap.set('n', 'gE', vim.diagnostic.goto_prev)
 -- vim.keymap.set('n', 'ge', vim.diagnostic.goto_next)
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
-vim.keymap.set("n", "<c-f>", "za") -- toggle folds
+-- Edit configuration files
+vim.keymap.set("n", "<leader>ev", ":edit $MYVIMRC<cr>")
+vim.keymap.set("n", "<leader>et", ":edit ~/.tmux.conf<cr>")
+vim.keymap.set("n", "<leader>ez", ":edit ~/.zshrc<cr>")
+vim.keymap.set("n", "<leader>eh", ":edit ~/.hammerspoon/init.lua<cr>")
 
 -- emacs style buffer movement
 vim.keymap.set("n", "<leader>bn", ":bn<cr>")
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,6 +1,5 @@
 -- [[Vim Options]]
--- vim.o.lazyredrew = true -- improve performance?
-vim.o.hlsearch = true -- Set highlight on search
+
 vim.o.number = true -- Make line numbers default
 -- vim.o.relativenumber = true -- show relative line numbers
 vim.o.wrap = false -- Don't wrap lines
@@ -9,10 +8,14 @@ vim.o.updatetime = 250 -- Decrease update time
 vim.o.signcolumn = "yes" -- always show sign column
 vim.o.completeopt = "menuone,noselect" -- better completion experience
 vim.o.mouse = "a" -- Enable mouse moedwardbaeg9@gmail.com@de
-vim.wo.cursorline = true -- highlight line with cursor, window scoped for use with reticle.nvim
+vim.o.cursorline = true -- highlight line with cursor, window scoped for use with reticle.nvim
+vim.o.hidden = true -- allow switching buffers without saving
+vim.o.scrolloff = 24 -- number of lines to keep above/below the cursor
+vim.o.linebreak = true -- wrap at spaces instead of the middle of a word
 
 vim.o.ignorecase = true -- case insensitive searching
 vim.o.smartcase = true -- ...unless /C or capital in search
+vim.o.inccommand = "nosplit" -- show likve search and replace
 
 vim.o.undofile = true -- Save undo history
 vim.o.undodir = vim.fn.expand("~/.vim/undo") -- set save directory. This must exist first... I think
@@ -20,16 +23,23 @@ vim.o.undodir = vim.fn.expand("~/.vim/undo") -- set save directory. This must ex
 vim.o.list = true -- show types of whitespace
 vim.o.listchars = "tab:‣ ,trail:•,precedes:«,extends:»"
 
-vim.o.hidden = true -- allow switching buffers without saving
-
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.expandtab = true
 
+vim.o.spelllang = "en"
+vim.o.spellsuggest = "best,9"
+
+vim.o.splitright = true -- open splits on the right
+vim.o.splitbelow = true -- open splits on the bottom
+
+-- vim.o.showmatch = true -- briefly flash matching bracket -- replaced with a plugin
+-- vim.o.matchtime = 2 = multiple of 100ms
+
+-- set tabstop for specific filetypes
 vim.api.nvim_create_autocmd("FileType", {
    pattern = "sh",
    callback = function()
-      vim.api.nvim_buf_set_option(0, "tabstop", 4)
       vim.api.nvim_buf_set_option(0, "tabstop", 4)
    end,
 })
@@ -47,10 +57,10 @@ vim.api.nvim_create_autocmd("FileType", {
 -- augroup END
 -- ]])
 
--- [[ Smart clipboard ]]
+-- set clipboard per os
 vim.cmd([[
   if has("win32")
-    echo "is this windows?"
+    " echo "is this windows?"
     set clipboard=unnamed " integrate with windows
   else
     if has("unix")
@@ -60,6 +70,24 @@ vim.cmd([[
       endif
     endif
   endif
+]])
+
+-- View output from running in terminal
+vim.cmd([[
+noremap <A-b> :call Build() <cr>
+function! Build()
+  if &filetype == "javascript"
+    exec "! node %"
+  elseif &filetype == 'typescript'
+    exec "!ts-node %"
+  elseif &filetype == 'lua'
+    exec "!lua %"
+  elseif &filetype == "python"
+  elseif &filetype == "sh"
+  elseif &filetype == "sh"
+    exec "!bash %"
+  endif
+endfunction
 ]])
 
 -- remap netrw keymaps
@@ -82,6 +110,7 @@ vim.api.nvim_create_autocmd("filetype", {
 vim.api.nvim_create_user_command("PrintFile", "echo @%", { desc = "Show the path for the current file" })
 
 -- show cursor line only in active window
+-- this doesn't work with <c-c>, only with <esc>
 -- https://github.com/folke/dot/blob/master/nvim/lua/config/autocmds.lua
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
    callback = function()
@@ -104,35 +133,22 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 
 -- [[ Highlights ]]
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1c1c1c" }) -- set background color of floating windows; plugins: telescope, which-key
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#546178", bg = "#1c1c1c" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#546178", bg = "#1c1c1c" }) -- border of floating windows
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#101010" }) -- darker cursorline
 vim.api.nvim_set_hl(0, "MatchParen", { fg = "#ffffff" }) -- make matching parens easier to see
 -- vim.api.nvim_set_hl(0, "Whitespace", { fg = "#ffffff" }) -- TODO: only highlight trailing whitespace
 
-vim.api.nvim_set_hl(0, "@operator", { italic = false, fg = "#99d1db" }) -- eg +, =, || only do for js?
-vim.api.nvim_set_hl(0, "@variable.builtin", { italic = true, fg = "#e78284" }) -- eg +, =, || only do for js?
+vim.api.nvim_set_hl(0, "@operator", { italic = false, fg = "#99d1db" }) -- eg +, =, || -- TODO?: only do for js?
+vim.api.nvim_set_hl(0, "@variable.builtin", { italic = true, fg = "#e78284" }) -- eg +, =, || -- TODO?: only do for js?
 -- vim.api.nvim_set_hl(0, '@keyword.function', { italic = true }) -- highlights the keyword 'function'
 -- vim.api.nvim_set_hl(0, 'Keyword', { italic = true }) -- highlights the keyword 'function'
 -- vim.api.nvim_set_hl(0, '@method.call', { italic = false }) -- highlights the keyword 'Instance.method'
 
 -- [[ Legacy config syntax ]]
 vim.cmd([[
-" set showmatch " show matching bracket
-" set matchtime=2 " multiple of 100ms
-
-set scrolloff=24 " buffer top and bottom
-set linebreak " don't break in the middle of a word
-
-set incsearch " search realtime
-set hlsearch
-nnoremap <silent> <Leader><Space> :nohlsearch<Bar>:echo<CR>
-set ignorecase
-set smartcase
-set inccommand=nosplit " live substitutions
 
 " Center after jumps
 nnoremap g; g;zz
-" nnoremap gi gi<esc>zzi
 
 " center after search "don't really need this with scrolloffset
 " nnoremap n nzz
@@ -141,46 +157,4 @@ nnoremap g; g;zz
 " nnoremap # #zz
 " nnoremap g* g*zz
 " nnoremap g# g#zz
-
-" Remap move cursor to ends of lines H / L
-noremap <S-h> ^
-noremap <S-l> $
-
-" Quick edit configs
-nnoremap <leader>ev :edit $MYVIMRC<cr>
-nnoremap <leader>et :edit ~/.tmux.conf<cr>
-nnoremap <leader>ez :edit ~/.zshrc<cr>
-nnoremap <leader>eh :edit ~/.hammerspoon/init.lua<cr>
-
-" Split into two lines
-" nnoremap K i<CR><ESC>
-nnoremap <leader><cr> i<CR><ESC>
-
-" Make Y consistent with C and D, until EOL
-nnoremap Y y$
-
-" Visual select previously pasted text
-nnoremap gp `[v`]
-
-" View output from running in terminal
-noremap <A-b> :call Build() <cr>
-function! Build()
-  if &filetype == "javascript"
-    exec "! node %"
-  elseif &filetype == 'typescript'
-    exec "!ts-node %"
-  elseif &filetype == 'lua'
-    exec "!lua %"
-  elseif &filetype == "python"
-  elseif &filetype == "sh"
-  elseif &filetype == "sh"
-    exec "!bash %"
-  endif
-endfunction
-
-set spelllang=en
-set spellsuggest=best,9
-
-set splitright " open splits on the right
-set splitbelow " open splits on the bottom
 ]])
