a34a5def05719232367959cec23329978b17d8f5
edwardbaeg <edwardbaeg9@gmail.com>
2023-08-22 17:05:43 -0500

    Add visual bell

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,6 +18,15 @@ config.colors = {
    cursor_bg = "#008080",
    cursor_fg = "#ffffff",
    background = "#1e1e1e",
+   visual_bell = "#202020",
+}
+
+-- Makes the background flash for visual bell
+config.visual_bell = {
+   fade_in_function = "EaseIn",
+   fade_in_duration_ms = 150,
+   fade_out_function = "EaseOut",
+   fade_out_duration_ms = 150,
 }
 
 config.window_background_opacity = 0.95
@@ -25,8 +34,8 @@ config.macos_window_background_blur = 25
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
--- config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 config.font = wezterm.font("Operator Mono")
+-- config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 -- config.font = wezterm.font("Nerd Font Symbols", { weight = "Medium" })
 config.font_size = 16
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable ligatures
