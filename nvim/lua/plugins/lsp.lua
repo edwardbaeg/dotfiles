35f72b2408b6d3d4ff88df589fd36cc2d5b45f71
edwardbaeg <edwardbaeg9@gmail.com>
2023-09-18 11:29:19 -0400

    Add symbol-usage

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -357,4 +357,15 @@ return {
          })
       end,
    },
+
+   {
+      -- display usage counts for document symbols
+      "Wansmer/symbol-usage.nvim",
+      event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+      config = function()
+         require("symbol-usage").setup({
+            vt_position = "end_of_line",
+         })
+      end,
+   },
 }
