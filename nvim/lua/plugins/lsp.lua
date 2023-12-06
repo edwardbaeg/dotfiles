b0872ebe614456504790b05ec59e2a1fbacbde0a
edwardbaeg <edwardbaeg9@gmail.com>
2023-12-05 22:45:43 -0500

    Add wtf.nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -383,4 +383,32 @@ return {
          })
       end,
    },
+
+   {
+      -- debug with AI
+      -- NOTE: this requires env var OPENAI_API_KEY
+      "piersolenski/wtf.nvim",
+      dependencies = {
+         "MunifTanjim/nui.nvim",
+      },
+      opts = {},
+      keys = {
+         {
+            "gw",
+            mode = { "n", "x" },
+            function()
+               require("wtf").ai()
+            end,
+            desc = "Debug diagnostic with AI",
+         },
+         {
+            mode = { "n" },
+            "gW",
+            function()
+               require("wtf").search()
+            end,
+            desc = "Search diagnostic with Google",
+         },
+      },
+   },
 }
