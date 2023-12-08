de39d88e31965175161c24e2d4233e35ff38d0b2
edwardbaeg <edwardbaeg9@gmail.com>
2023-12-07 23:46:24 -0500

    Update lspsaga keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -259,8 +259,10 @@ return {
                end,
             },
             mapping = cmp.mapping.preset.insert({
-               ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-               ["<C-f>"] = cmp.mapping.scroll_docs(4),
+               ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+               ["<C-j>"] = cmp.mapping.scroll_docs(4),
+               -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+               -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
                -- ['<C-Space>'] = cmp.mapping.complete(),
                ["<C-l>"] = cmp.mapping.abort(),
                ["<CR>"] = cmp.mapping.confirm({
