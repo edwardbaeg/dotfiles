e50fa27b3ea512e4299ec1aa1e3e6109de4787a2
edwardbaeg <edwardbaeg9@gmail.com>
2024-01-22 22:26:39 -0500

    Add autocmds

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -137,6 +137,56 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
    end,
 })
 
+-- From lazyvim
+local function augroup(name)
+   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+   group = augroup("checktime"),
+   callback = function()
+      if vim.o.buftype ~= "nofile" then
+         vim.cmd("checktime")
+      end
+   end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+   group = augroup("resize_splits"),
+   callback = function()
+      local current_tab = vim.fn.tabpagenr()
+      vim.cmd("tabdo wincmd =")
+      vim.cmd("tabnext " .. current_tab)
+   end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+   group = augroup("close_with_q"),
+   pattern = {
+      "PlenaryTestPopup",
+      "help",
+      "lspinfo",
+      "man",
+      "notify",
+      "qf",
+      "query",
+      "spectre_panel",
+      "startuptime",
+      "tsplayground",
+      "neotest-output",
+      "checkhealth",
+      "neotest-summary",
+      "neotest-output-panel",
+   },
+   callback = function(event)
+      vim.bo[event.buf].buflisted = false
+      vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+   end,
+})
+
 -- [[ Highlights ]]
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1c1c1c" }) -- set background color of floating windows; plugins: telescope, which-key
 -- vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#546178", bg = "#1c1c1c" }) -- border of floating windows
