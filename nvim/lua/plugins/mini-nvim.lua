517f170324dd62634912020212f88a19accdb428
edwardbaeg <edwardbaeg9@gmail.com>
2024-02-05 12:29:21 -0500

    Update comments

--- nvim/lua/plugins/mini-nvim.lua
+++ nvim/lua/plugins/mini-nvim.lua
@@ -3,36 +3,35 @@ return {
       -- a collection of mini 'submodules'
       "echasnovski/mini.nvim",
       config = function()
-         require("mini.move").setup({}) -- adds ability to move text around with <m-h/j/k/l>
+         -- adds ability to move text around with <m-h/j/k/l>
+         require("mini.move").setup({})
 
-         require("mini.cursorword").setup({ -- highlights/underlines the word under the cursor
+         -- highlights/underlines the word under the cursor
+         require("mini.cursorword").setup({
             delay = 500, -- in ms
          })
 
+         -- file explorer as a buffer
          require("mini.files").setup({
-            -- file explorer
             windows = {
                preview = true,
             },
-
             options = {
                use_as_default_explorer = false,
             },
          })
-
          -- open with focus on the current file
          vim.api.nvim_create_user_command("MiniFiles", "lua MiniFiles.open(vim.api.nvim_buf_get_name(0))", {})
          vim.api.nvim_create_user_command("Files", "lua MiniFiles.open(vim.api.nvim_buf_get_name(0))", {})
 
-         -- require("mini.pairs").setup({}) -- automatic pair closing
-
-         -- Default mappings:
-         -- g= -> evaluate
-         -- gx -> exchange
-         -- gm -> multiply/duplicate
-         -- gr -> replace
-         -- gs -> sort
+         -- Add various operators
          require("mini.operators").setup({
+            -- Default mappings:
+            -- g= -> evaluate
+            -- gx -> exchange
+            -- gm -> multiply/duplicate
+            -- gr -> replace
+            -- gs -> sort
             exchange = {
                prefix = "gX",
             },
@@ -41,34 +40,20 @@ return {
             },
          })
 
+         -- smooth scrolling
          local animate = require("mini.animate")
          animate.setup({
-            cursor = {
-               enable = false,
-            },
             scroll = {
-               -- enable = false,
-               -- timing = animate.gen_timing.linear({
-               --    duration = 5,
-               -- }),
                timing = animate.gen_timing.linear({
-                  -- easing = "in",
                   easing = "out",
-                  -- duration = 5,
-                  -- duration = 150,
                   duration = 100,
                   unit = "total",
                }),
             },
-            resize = {
-               enable = false,
-            },
-            open = {
-               enable = false,
-            },
-            close = {
-               enable = false,
-            },
+            cursor = { enable = false },
+            resize = { enable = false },
+            open = { enable = false },
+            close = { enable = false },
          })
       end,
    },
