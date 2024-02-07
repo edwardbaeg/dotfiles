bc854b5ea7adab7aca3368c18aa0aca72b5e2bab
edwardbaeg <edwardbaeg9@gmail.com>
2024-02-07 10:29:56 -0500

    Split fuzzy plugins into files

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,29 @@
+return {
+   "ibhagwan/fzf-lua",
+   dependencies = { "nvim-tree/nvim-web-devicons" },
+   event = "VeryLazy",
+   config = function()
+      require("fzf-lua").setup({
+         -- "fzf-tmux",
+         "default",
+         winopts = {
+            height = 0.9, -- default is 0.85
+            width = 0.9, -- default is 0.80
+            row = 0.35, -- default is 0.35
+            col = 0.5, -- default is 0.50
+            preview = {
+               layout = "flex",
+               flip_columns = 150, -- number of columns at which to flip to horizontal, default is 120
+               horizontal = "right:50%",
+            },
+         },
+      })
+   end,
+   init = function()
+      vim.keymap.set("n", "<c-g>", "<cmd>lua require('fzf-lua').grep_project()<cr>", { silent = true })
+      vim.keymap.set("n", "<c-b>", "<cmd>lua require('fzf-lua').buffers()<cr>", { silent = true })
+      vim.keymap.set("n", "<c-p>", "<cmd>lua require('fzf-lua').files()<cr>", { silent = true })
+
+      vim.keymap.set("n", "<leader>*", "<cmd>lua require('fzf-lua').grep_cword()<cr>", { silent = true })
+   end,
+}
