f0a8c8d2a9afb974692c6d3ce79e313331be02a0
edwardbaeg <edwardbaeg9@gmail.com>
2024-02-27 17:15:40 -0500

    Update incline

--- nvim/lua/plugins/set-ui.lua
+++ nvim/lua/plugins/set-ui.lua
@@ -251,10 +251,28 @@ return {
    },
 
    {
+      -- floating statusline
       "b0o/incline.nvim",
       event = "VeryLazy",
       config = function()
-         require("incline").setup({})
+         require("incline").setup({
+            window = {
+               padding = 0,
+               margin = {
+                  horizontal = 0,
+               },
+            },
+            render = function(props)
+               local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+               if filename == "" then
+                  filename = "[No Name]"
+               end
+               local modified = vim.bo[props.buf].modified
+               return {
+                  { filename .. (modified and " [+]" or ""), gui = modified and "bold,italic" or "bold" },
+               }
+            end,
+         })
       end,
    },
 }
