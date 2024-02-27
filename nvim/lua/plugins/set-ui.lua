02039fd7b05e9d1f7bfb265c90b50accc729a804
edwardbaeg <edwardbaeg9@gmail.com>
2024-02-26 23:50:05 -0500

    Move bufferline

--- nvim/lua/plugins/set-ui.lua
+++ nvim/lua/plugins/set-ui.lua
@@ -55,104 +55,6 @@ return {
    },
 
    {
-      -- fancier tabline
-      "akinsho/bufferline.nvim",
-      dependencies = "nvim-tree/nvim-web-devicons",
-      enabled = not vim.g.vscode,
-      config = function()
-         local background_color = "#151515" -- dark gray
-         local dim_color = "#1a1a1a" -- dark gray
-         -- local teal = "#00ffff"
-
-         require("bufferline").setup({
-            options = {
-               numbers = function(opts)
-                  -- return string.format("%s·%s", opts.raise(opts.ordinal), opts.lower(opts.id))
-                  return opts.raise(opts.ordinal)
-               end,
-               show_buffer_close_icons = false,
-               show_close_icon = false,
-               modified_icon = "•",
-               -- separator_style = { "", "" }, -- no separators
-               separator_style = "slant",
-               indicator = {
-                  -- style = "underline",
-               },
-               offsets = {
-                  {
-                     filetype = "neo-tree",
-                     text = "",
-                     highlight = "Directory",
-                     separator = true, -- use a "true" to enable the default, or set your own character
-                  },
-                  {
-                     filetype = "NvimTree",
-                     text = "",
-                     highlight = "Directory",
-                     separator = true, -- use a "true" to enable the default, or set your own character
-                  },
-               },
-            },
-            highlights = {
-               -- the background of the whole bar
-               fill = {
-                  bg = background_color,
-               },
-
-               -- active buffer
-               buffer_selected = {
-                  bold = true,
-                  italic = false,
-                  fg = "#ffffff",
-               },
-               -- for inactive tabs
-               background = {
-                  bg = dim_color,
-               },
-               -- for visible tabs, but inactive
-               buffer_visible = {
-                  bold = false,
-                  italic = false,
-                  fg = "#b1b1b1",
-               },
-
-               -- separator for active tab
-               separator_selected = {
-                  fg = background_color,
-               },
-               -- separator for inactive tabs
-               separator = {
-                  fg = background_color,
-                  bg = dim_color,
-               },
-               -- separtor for visible tabs, but not active
-               separator_visible = {
-                  fg = background_color,
-               },
-
-               -- numbers in backgrund background buffers
-               numbers = {
-                  bg = dim_color,
-               },
-
-               -- modified icon for active tab
-               modified_selected = {
-                  fg = "#FFFF00",
-               },
-               -- modified icon for inactive tabs
-               modified = {
-                  fg = "#FFFF00",
-               },
-               -- modified icon for inactive but visible tabs
-               modified_visible = {
-                  fg = "#FFFF00",
-               },
-            },
-         })
-      end,
-   },
-
-   {
       -- Add indentation guides
       "lukas-reineke/indent-blankline.nvim",
       enabled = not vim.g.vscode,
@@ -301,12 +203,16 @@ return {
       "nvim-zh/colorful-winsep.nvim",
       enabled = not vim.g.vscode,
       config = function()
+         -- local frappe = require("catppuccin.palettes").get_palette("frappe")
          require("colorful-winsep").setup({
             highlight = {
                bg = "none",
-               fg = "#00667c",
+               fg = "cyan4",
             },
-            interval = 1000,
+            integrations = {
+               bufferline = true,
+            },
+            -- interval = 1000,
          })
       end,
    },
@@ -354,6 +260,12 @@ return {
          require("reactive").add_preset({
             name = "custom",
             modes = {
+               n = {
+                  -- highlight CursorLine only in a current window, not in all
+                  winhl = {
+                     CursorLineNr = { bg = "darkslategray" },
+                  },
+               },
                no = {
                   operators = {
                      -- switch case
