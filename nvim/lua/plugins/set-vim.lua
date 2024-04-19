ef202e5cf5bb4735b0330aaa7b41296a37ea05e1
edwardbaeg <edwardbaeg9@gmail.com>
2024-04-19 17:56:13 -0400

    Add yankbank keymap

--- nvim/lua/plugins/set-vim.lua
+++ nvim/lua/plugins/set-vim.lua
@@ -181,7 +181,7 @@ return {
       "ptdewey/yankbank-nvim",
       config = function()
          require("yankbank").setup()
-         -- vim.keymap.set("n", "<leader>y", "<cmd>YankBank<CR>", { noremap = true })
+         vim.keymap.set("n", "<c-y>", "<cmd>YankBank<CR>", { noremap = true })
       end,
    },
 
