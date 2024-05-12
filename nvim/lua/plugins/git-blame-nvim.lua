151513d5c08487e5c1e3b642f2737d20f2ef75ee
edwardbaeg <edwardbaeg9@gmail.com>
2024-05-12 13:55:51 -0400

    Update git-blame

--- nvim/lua/plugins/git-blame-nvim.lua
+++ nvim/lua/plugins/git-blame-nvim.lua
@@ -4,7 +4,8 @@ return {
    config = function()
       require("gitblame").setup({})
       -- vim.g.gitblame_message_template = "<author> • <summary> • <date>"
-      vim.g.gitblame_message_template = "<author>, <summary>, <date>"
+      -- vim.g.gitblame_message_template = "<author>, <summary>, <date>"
+      vim.g.gitblame_message_template = "<author>, <date>"
       vim.g.gitblame_date_format = "%r" -- relative date
       vim.g.gitblame_message_when_not_committed = "Not committed"
    end,
