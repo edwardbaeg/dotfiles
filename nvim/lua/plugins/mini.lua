09f32beaaa33a0ec9255188734d588c0cabbb21e
edwardbaeg <edwardbaeg9@gmail.com>
2024-08-13 00:21:25 -0400

    Update mini.files

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -30,6 +30,37 @@ return {
          -- open with focus on the current file
          vim.api.nvim_create_user_command("MiniFiles", "lua MiniFiles.open(vim.api.nvim_buf_get_name(0))", {})
          vim.api.nvim_create_user_command("Files", "lua MiniFiles.open(vim.api.nvim_buf_get_name(0))", {})
+         vim.api.nvim_create_user_command("MF", "lua MiniFiles.open(vim.api.nvim_buf_get_name(0))", {})
+
+         -- FIXME: this doesn't work, just makes a split of the current buffer
+         -- Sourced from minifiles helpfile, create keyamp `L` to open file in a vertical split
+         local MiniFiles = require("mini.files")
+         local map_split = function(buf_id, lhs, direction)
+            local rhs = function()
+               -- Make new window and set it as target
+               local new_target_window
+               vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+                  vim.cmd(direction .. " split")
+                  new_target_window = vim.api.nvim_get_current_win()
+               end)
+
+               MiniFiles.set_target_window(new_target_window)
+            end
+
+            -- Adding `desc` will result into `show_help` entries
+            local desc = "Split " .. direction
+            vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+         end
+
+         vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+               local buf_id = args.data.buf_id
+               -- Tweak keys to your liking
+               -- map_split(buf_id, "gs", "belowright horizontal")
+               map_split(buf_id, "L", "belowright vertical")
+            end,
+         })
 
          -- Add various operators
          require("mini.operators").setup({
