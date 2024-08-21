6814eb93e4bbc1efc966d5026fa4ee823274230b
edwardbaeg <edwardbaeg9@gmail.com>
2024-08-21 11:37:17 -0400

    Add fzfluz keymaps

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -48,5 +48,6 @@ return {
       vim.keymap.set("n", "<leader>fb", "<cmd>lua require('fzf-lua').git_bcommits()<cr>", { silent = true })
       vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').builtin()<cr>", { silent = true })
       vim.keymap.set("n", "<leader>fj", "<cmd>lua require('fzf-lua').jumps()<cr>", { silent = true })
+      vim.keymap.set("n", "<leader>fl", "<cmd>lua require('fzf-lua').lines()<cr>", { silent = true })
    end,
 }
