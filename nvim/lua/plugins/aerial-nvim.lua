cb21f05e6e497a73895df7bb09ee6a5dc97f8ee0
edwardbaeg <edwardbaeg9@gmail.com>
2024-09-03 10:20:16 -0400

    Add aerial

---
+++ nvim/lua/plugins/aerial-nvim.lua
@@ -0,0 +1,15 @@
+return {
+   "stevearc/aerial.nvim",
+   opts = {},
+   -- Optional dependencies
+   dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+   },
+   config = function()
+      require("aerial").setup()
+
+      vim.keymap.set("n", "<leader>at", "<cmd>AerialToggle<CR>", { desc = "Aerial [T]oggle" })
+      vim.keymap.set("n", "<leader>an", "<cmd>AerialNavToggle<CR>", { desc = "Aerial [N]av toggle" })
+   end,
+}
