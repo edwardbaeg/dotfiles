da84ce4dfb712a684162de2556a92804fb89255b
edwardbaeg <edwardbaeg9@gmail.com>
2024-10-02 20:33:16 -0400

    Add multicursor

--- nvim/lua/plugins/set-coding.lua
+++ nvim/lua/plugins/set-coding.lua
@@ -160,11 +160,11 @@ return {
       event = "VeryLazy", -- this is for making sure our keymaps are applied after the others: we call the previous mapppings, but other plugins/configs usually not!
    },
 
-   {
-      -- multiple cursors mode
-      -- use <ctrl-n> to add multiple cursors
-      "mg979/vim-visual-multi",
-   },
+   -- {
+   --    -- multiple cursors mode
+   --    -- use <ctrl-n> to add multiple cursors
+   --    "mg979/vim-visual-multi",
+   -- },
 
    {
       -- jump to any location
