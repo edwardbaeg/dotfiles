b3f7075191e3873a04745dc46d64e5e3d7932f9e
edwardbaeg <edwardbaeg9@gmail.com>
2024-10-02 20:33:05 -0400

    diffview: update

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -5,12 +5,21 @@ return {
    config = function()
       require("diffview").setup()
 
+      -- TODO: add g to each keymap
       vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<cr>", { noremap = true })
       vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", { noremap = true })
       vim.keymap.set("n", "<leader>dq", "<cmd>DiffviewClose<cr>", { noremap = true })
+      vim.keymap.set("n", "<leader>gdq", "<cmd>DiffviewClose<cr>", { noremap = true })
       vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory %<cr>", { noremap = true })
       vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", { noremap = true })
       vim.keymap.set("n", "<leader>dm", "<cmd>DiffviewOpen master..HEAD<cr>", { noremap = true })
+      vim.keymap.set("n", "<leader>gdm", "<cmd>DiffviewOpen master..HEAD<cr>", { noremap = true })
+
+      local actions = require("diffview.actions")
+      vim.keymap.set("n", "<leader>dn", actions.next_conflict, { noremap = true, desc = "[n]ext conflict" })
+      vim.keymap.set("n", "<leader>gdn", actions.next_conflict, { noremap = true, desc = "[n]ext conflict" })
+      vim.keymap.set("n", "<leader>dp", actions.prev_conflict, { noremap = true, desc = "[p]revious conflict" })
+      vim.keymap.set("n", "<leader>gdp", actions.prev_conflict, { noremap = true, desc = "[p]revious conflict" })
 
       local function diffOpenWithInput()
          -- local user_input = vim.fn.input("Revision to Open: ")
@@ -19,6 +28,9 @@ return {
             vim.cmd("DiffviewOpen " .. input)
          end)
       end
+
+      -- ignore changes in the amount of whitespace
+      vim.o.diffopt = "iwhite"
 
       require("which-key").add({
          {
