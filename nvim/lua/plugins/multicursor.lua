807b93992416f3eb0eb3c40ed867729833590e12
edwardbaeg <edwardbaeg9@gmail.com>
2024-10-16 19:38:42 -0500

    fix: multicursor add cursor keybind

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -8,6 +8,23 @@ return {
 
       local set = vim.keymap.set
 
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ "n", "v" }, "<leader>nc", function()
+         mc.matchAddCursor(1)
+      end)
+      set({ "n", "v" }, "<c-n>", function()
+         mc.matchAddCursor(1)
+      end)
+      set({ "n", "v" }, "<leader>s", function()
+         mc.matchSkipCursor(1)
+      end)
+      set({ "n", "v" }, "<leader>N", function()
+         mc.matchAddCursor(-1)
+      end)
+      set({ "n", "v" }, "<leader>S", function()
+         mc.matchSkipCursor(-1)
+      end)
+
       -- Add or skip cursor above/below the main cursor.
       set({ "n", "v" }, "<up>", function()
          mc.lineAddCursor(-1)
@@ -20,21 +37,6 @@ return {
       end)
       set({ "n", "v" }, "<leader><down>", function()
          mc.lineSkipCursor(1)
-      end)
-
-      -- Add or skip adding a new cursor by matching word/selection
-      -- set({ "n", "v" }, "<leader>n", function()
-      set({ "n", "v" }, "<c-n>", function()
-         mc.matchAddCursor(1)
-      end)
-      set({ "n", "v" }, "<leader>s", function()
-         mc.matchSkipCursor(1)
-      end)
-      set({ "n", "v" }, "<leader>N", function()
-         mc.matchAddCursor(-1)
-      end)
-      set({ "n", "v" }, "<leader>S", function()
-         mc.matchSkipCursor(-1)
       end)
 
       -- You can also add cursors with any motion you prefer:
