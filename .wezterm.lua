0f5e9a8d4a2165bc3f030cd3bdcc087b50735508
edwardbaeg <edwardbaeg9@gmail.com>
2024-11-04 01:22:11 -0500

    Bump line height

--- .wezterm.lua
+++ .wezterm.lua
@@ -37,7 +37,8 @@ config.macos_window_background_blur = 75
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.line_height = 1
+-- config.line_height = 1
+config.line_height = 1.05
 
 config.font = wezterm.font("Operator Mono", { weight = "Book" })
 -- config.font = wezterm.font("Operator Mono", { weight = "Medium" })
