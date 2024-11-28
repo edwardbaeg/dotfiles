a5df06b5eea3825e43cc35026202162965fb2cd2
edwardbaeg <edwardbaeg9@gmail.com>
2024-11-27 21:33:43 -0600

    Add smear-cursor

--- nvim/lua/plugins/set-ui.lua
+++ nvim/lua/plugins/set-ui.lua
@@ -211,4 +211,16 @@ return {
          -- },
       },
    },
+
+   {
+      -- animated cursor smear effect
+      "sphamba/smear-cursor.nvim",
+      opts = {
+         stiffness = 1, -- 0.6      [0, 1]
+         trailing_stiffness = 0.6, -- 0.3      [0, 1]
+         trailing_exponent = 0, -- 0.1      >= 0
+         distance_stop_animating = 0.5, -- 0.1      > 0
+         hide_target_hack = false, -- true     boolean
+      },
+   },
 }
