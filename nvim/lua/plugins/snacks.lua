30db5a99f4859aef62443ddaf719037b60a5e597
edwardbaeg <edwardbaeg9@gmail.com>
2024-12-12 18:06:40 -0500

    Replace smooth scrolling with snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,17 +1,33 @@
----@diagnostic disable: undefined-global the plugin adds Snacks as a global variable
+-- -@diagnostic disable: undefined-global the plugin adds Snacks as a global variable
 return {
    -- TODO consider using bigfile, gitbrowse, notify, notifier, rename,
    "folke/snacks.nvim",
    lazy = false,
    priority = 1000,
-   enable = false,
-   keys = {
-      {
-         "<leader>lg",
-         function()
-            Snacks.lazygit()
-         end,
-         desc = "lazygit",
+   opts = {
+      animate = {
+         fps = 144,
       },
+      scroll = {
+      ---@class snacks.animate.Config
+         animate = {
+            -- The minimum of duration or duration per step will be used
+            duration = {
+               -- step = 15,
+               total = 75,
+            },
+            easing = "linear",
+         }
+      },
+   },
+   keys = {
+      -- prefer toggle term for better
+      -- {
+      --    "<leader>lg",
+      --    function()
+      --       Snacks.lazygit()
+      --    end,
+      --    desc = "lazygit",
+      -- },
    },
 }
