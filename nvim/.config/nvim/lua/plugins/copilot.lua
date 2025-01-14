a2986dcd37cf8f3f6cdd093ce1cc269d4ebba011
edwardbaeg <edwardbaeg9@gmail.com>
2025-01-14 11:09:24 -0500

    Feat: add copilot chat

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -34,4 +34,24 @@ return {
          })
       end,
    },
+
+   {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      dependencies = {
+         { "zbirenbaum/copilot.lua" },
+         { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      },
+      build = "make tiktoken", -- Only on MacOS or Linux
+      opts = {},
+      keys = {
+         {
+            "<leader>ccp",
+            function()
+               local actions = require("CopilotChat.actions")
+               require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
+            end,
+            desc = "CopilotChat - Prompt actions",
+         },
+      },
+   },
 }
