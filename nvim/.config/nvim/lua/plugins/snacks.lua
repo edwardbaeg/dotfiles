c597891f3b76a5776c97ccff1fb631c32acf3a42
edwardbaeg <edwardbaeg9@gmail.com>
2025-01-13 23:29:25 -0500

    Feat: add snacks.scratch

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,17 +1,38 @@
 ---@diagnostic disable: undefined-global the plugin adds Snacks as a global variable
 return {
-   -- TODO consider using bigfile, gitbrowse, notify, notifier, rename,
+   -- TODO consider using bigfile, gitbrowse, notify, notifier, rename
+   -- Lazygit opens files as buffers but doesn't support hiding terminal during async functions
    "folke/snacks.nvim",
    lazy = false,
    priority = 1000,
-   enable = false,
    keys = {
+      -- {
+      --    "<c-\\>",
+      --    function()
+      --       Snacks.lazygit()
+      --    end,
+      --    desc = "lazygit",
+      -- },
+      -- {
+      --    "<leader>lg",
+      --    function()
+      --       Snacks.lazygit()
+      --    end,
+      --    desc = "lazygit",
+      -- },
       {
-         "<leader>lg",
+         "<leader>.",
          function()
-            Snacks.lazygit()
+            Snacks.scratch()
          end,
-         desc = "lazygit",
+         desc = "Toggle Scratch Buffer",
       },
    },
+   config = function(_, opts)
+      Snacks.setup(opts)
+
+      vim.api.nvim_create_user_command("Scratch", function()
+         Snacks.scratch()
+      end, {})
+   end,
 }
