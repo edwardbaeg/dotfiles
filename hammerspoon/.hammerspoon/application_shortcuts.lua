501c49d1b025b56fb3fb28d5778f38fd6713f607
edwardbaeg <edwardbaeg9@gmail.com>
2025-01-28 16:01:41 -0500

    Fix: only register keymap when linear is focused

--- hammerspoon/.hammerspoon/application_shortcuts.lua
+++ hammerspoon/.hammerspoon/application_shortcuts.lua
@@ -1,20 +1,34 @@
 -- App specific keybindings
-function _G.LinearCopyUrl()
-   local linearApp = hs.application.find("Linear")
-   if (linearApp) then
-      linearApp:activate()
-      hs.eventtap.keyStroke({ "cmd", "shift" }, "c", linearApp)
+
+local M = {}
+
+function Linear()
+   function M.linearCopyUrl()
+      local linearApp = hs.application.find("Linear")
+      if linearApp then
+         linearApp:activate()
+         hs.eventtap.keyStroke({ "cmd", "shift" }, "c", linearApp)
+      end
    end
+
+   -- usage: hammerspoon://linearCopyUrl
+   hs.urlevent.bind("linearCopyUrl", function()
+      M.linearCopyUrl()
+   end)
+
+   local mapLinearCopyUrl = hs.hotkey.new({ " ctrl " }, "c", M.linearCopyUrl)
+   hs.window.filter
+      .new("Linear")
+      :subscribe(hs.window.filter.windowFocused, function()
+         mapLinearCopyUrl:enable()
+      end)
+      :subscribe(hs.window.filter.windowUnfocused, function()
+         mapLinearCopyUrl:disable()
+      end)
 end
 
 function Main()
-   -- usage: hammerspoon://linearCopyUrl
-   hs.urlevent.bind("linearCopyUrl", function()
-      LinearCopyUrl()
-   end)
-   hs.hotkey.bind({ "ctrl" }, "c", function()
-      LinearCopyUrl()
-   end)
+   Linear()
 end
 
 Main()
