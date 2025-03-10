8fc09c10877204fd90d2e7e980ec5557795de21e
edwardbaeg <edwardbaeg9@gmail.com>
2025-03-10 10:29:12 -0400

    Add hs.grid keymap

--- hammerspoon/.hammerspoon/window_grid.lua
+++ hammerspoon/.hammerspoon/window_grid.lua
@@ -2,13 +2,17 @@
 --------------------------------------------------------------------------
 local constants = require("common/constants")
 
+-- TODO: replace with hs.grid.setMargins
 hs.grid.MARGINX = 0
 hs.grid.MARGINY = 0
-hs.grid.GRIDWIDTH = 6
-hs.grid.GRIDHEIGHT = 4
 
 -- show interactive modal interface for resizing
 hs.hotkey.bind(constants.hyperkey, "G", function()
+   hs.grid.setGrid("6x4")
    hs.grid.show()
 end)
 
+hs.hotkey.bind(constants.hyperkey, "F", function()
+   hs.grid.setGrid("5x3")
+   hs.grid.show()
+end)
