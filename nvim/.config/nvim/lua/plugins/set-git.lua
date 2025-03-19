46f2dc78a4158d535cb05f46ff066df6c9315c66
edwardbaeg <edwardbaeg9@gmail.com>
2025-03-19 17:30:43 -0400

    Fix: gitlinker plugin setup

--- nvim/.config/nvim/lua/plugins/set-git.lua
+++ nvim/.config/nvim/lua/plugins/set-git.lua
@@ -1,35 +1,37 @@
 return {
    "gabebw/vim-github-link-opener", -- opens 'foo/bar' in github with gx
 
-   -- open current line in github permalink
-   "ruifm/gitlinker.nvim",
-   config = function()
-      require("gitlinker").setup({
-         -- mappings = "<leader>gg", -- default is gy
-      })
-      vim.keymap.set(
-         "n",
-         "<leader>gl",
-         '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-         { silent = true }
-      )
-      vim.keymap.set(
-         "v",
-         "<leader>gl",
-         '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-         { silent = true }
-      )
-      vim.api.nvim_create_user_command(
-         "Glink",
-         'lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})',
-         { desc = "Open github link in browser" }
-      )
-      vim.api.nvim_create_user_command(
-         "Gbrowse",
-         'lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})',
-         { desc = "Open github link in browser" }
-      )
-   end,
+   {
+      -- open current line in github permalink
+      "ruifm/gitlinker.nvim",
+      config = function()
+         require("gitlinker").setup({
+            -- mappings = "<leader>gg", -- default is gy
+         })
+         vim.keymap.set(
+            "n",
+            "<leader>gl",
+            '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+            { silent = true }
+         )
+         vim.keymap.set(
+            "v",
+            "<leader>gl",
+            '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+            { silent = true }
+         )
+         vim.api.nvim_create_user_command(
+            "Glink",
+            'lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})',
+            { desc = "Open github link in browser" }
+         )
+         vim.api.nvim_create_user_command(
+            "Gbrowse",
+            'lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})',
+            { desc = "Open github link in browser" }
+         )
+      end,
+   },
 
    {
       -- Adds git blame line as virtual text or in the status bar
