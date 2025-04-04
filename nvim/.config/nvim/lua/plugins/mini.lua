ea218ff068bb5e35f2d140a03e09da1fd4d85b94
edwardbaeg <edwardbaeg9@gmail.com>
2025-04-04 00:07:05 -0400

    Disable animate in neovide

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -95,22 +95,24 @@ return {
          -- NOTE: this breaks `gi`
          -- vim.g.minianimate_disable = true
          local animate = require("mini.animate")
-         animate.setup({
-            scroll = {
-               enable = not vim.g.vscode,
-               timing = animate.gen_timing.linear({
-                  easing = "out",
-                  -- duration = 100,
-                  -- duration = 50,
-                  duration = 75,
-                  unit = "total",
-               }),
-            },
-            cursor = { enable = false }, -- cursor path
-            resize = { enable = false }, -- window resize
-            open = { enable = false }, -- window opening
-            close = { enable = false }, -- window closing
-         })
+         if (not vim.g.neovide) then
+            animate.setup({
+               scroll = {
+                  enable = not vim.g.vscode,
+                  timing = animate.gen_timing.linear({
+                     easing = "out",
+                     -- duration = 100,
+                     -- duration = 50,
+                     duration = 75,
+                     unit = "total",
+                  }),
+               },
+               cursor = { enable = false }, -- cursor path
+               resize = { enable = false }, -- window resize
+               open = { enable = false }, -- window opening
+               close = { enable = false }, -- window closing
+            })
+         end
 
          -- track and reuse system visits
          require("mini.visits").setup({})
