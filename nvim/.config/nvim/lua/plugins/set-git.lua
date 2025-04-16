cfe294ca13d71bf649370a31b5cce11367688fa8
edwardbaeg <edwardbaeg9@gmail.com>
2025-04-16 12:27:36 -0400

    Update user command

--- nvim/.config/nvim/lua/plugins/set-git.lua
+++ nvim/.config/nvim/lua/plugins/set-git.lua
@@ -92,11 +92,11 @@ return {
    {
       -- Open git links
       -- currently only used for opening external links within nvim
-      -- :GitPortal open_link or :GitOpenLink
+      -- :GitPortal open_link or :GitPortalOpenLink
       "trevorhauter/gitportal.nvim",
       config = true,
       init = function()
-         vim.api.nvim_create_user_command("GitOpenLink", function()
+         vim.api.nvim_create_user_command("GitPortalOpenLink", function()
             require("gitportal").open_file_in_neovim()
          end, {})
       end,
