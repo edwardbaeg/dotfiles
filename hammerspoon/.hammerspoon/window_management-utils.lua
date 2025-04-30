3bd457e476cb82b4db2fcdc730d83bd82e778160
edwardbaeg <edwardbaeg9@gmail.com>
2025-04-30 16:09:43 -0400

    Refactor: abstract helpers

---
+++ hammerspoon/.hammerspoon/window_management-utils.lua
@@ -0,0 +1,141 @@
+local M = {}
+
+-- https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-1294971600
+-- Some applications (eg Firefox) animate resize instead of moving, due to AXEnhancedUserInterface
+-- So, temporarily disable it while moving the window
+local function axHotfix(win)
+   if not win then
+      win = hs.window.frontmostWindow()
+   end
+
+   local axApp = hs.axuielement.applicationElement(win:application())
+   if not axApp then
+      return
+   end
+   local wasEnhanced = axApp.AXEnhancedUserInterface
+   axApp.AXEnhancedUserInterface = false
+
+   return function()
+      hs.timer.doAfter(hs.window.animationDuration * 2, function()
+         axApp.AXEnhancedUserInterface = wasEnhanced
+      end)
+   end
+end
+
+-- TODO?: add branching logic for firefox, for potential performance
+M.withAxHotfix = function (fn, position)
+   if not position then
+      position = 1
+   end
+   local revert = axHotfix()
+   fn()
+   if revert then
+      revert()
+   end
+end
+
+local MARGIN = 4
+-- Buffer margin space around screen edges
+local function applyScreenEdgeMargins(window, screen)
+   local x, y, width, height = window.x, window.y, window.w, window.h
+   -- left edge
+   if x - screen.x < MARGIN then
+      width = width - MARGIN
+      x = screen.x + MARGIN
+   end
+   -- right edge
+   if screen.w - (x + width) < MARGIN then
+      width = width - MARGIN
+   end
+   -- top edge
+   if y - screen.y < MARGIN then
+      height = height - MARGIN
+      y = screen.y + MARGIN
+   end
+   -- bottom edge
+   if screen.h - (y + height) < MARGIN then
+      height = height - MARGIN
+   end
+   return { x = x, y = y, w = width, h = height }
+end
+
+local function withinMargin(a, b)
+   return math.abs(a - b) <= MARGIN * 2
+end
+
+---@param win hs.window
+---@param rect hs.geometry
+---@param duration number
+local function setFrame(win, rect, duration)
+   local newRect = applyScreenEdgeMargins(rect, win:screen():frame())
+   M.withAxHotfix(function()
+      win:setFrame(newRect, duration)
+   end)
+end
+
+---Cycles through a list of rects to set focused window position
+---@param options {h: number, w: number, x: number, y: number}[] list of rect to cycle through
+M.cyclePositions = function(options)
+   local win = hs.window.focusedWindow()
+   local frame = win:frame()
+
+   local function calculateDuration(appName, sameSize, sameHeight)
+      if appName == "Acrobat Reader" then
+         return 0
+      end
+      return sameSize and 0.15 or sameHeight and 0.1 or 0
+   end
+
+   local function matchesOption(rect, option)
+      return withinMargin(rect.h, option.h)
+         and withinMargin(rect.w, option.w)
+         and withinMargin(rect.x, option.x)
+         and withinMargin(rect.y, option.y)
+   end
+
+   local firstOption = options[1]
+   local nextOption = firstOption
+
+   for i, option in ipairs(options) do
+      if matchesOption(frame, option) then
+         nextOption = options[i + 1] or firstOption
+         break
+      end
+   end
+
+   local sameSize = withinMargin(frame.h, nextOption.h) and withinMargin(frame.w, nextOption.w)
+   local sameHeight = withinMargin(frame.h, nextOption.h)
+   local duration = calculateDuration(win:application():name(), sameSize, sameHeight)
+
+   setFrame(win, nextOption, duration)
+end
+
+---Converts a list of unit rects to screen rects
+---@param unitRects {x: number, y: number, w: number, h: number}[]
+---@return hs.geometry
+M.mapScreenUnitRects = function(unitRects)
+   local win = hs.window.focusedWindow()
+   local screenFrame = win:screen():frame()
+
+   ---@diagnostic disable-next-line: return-type-mismatch it's correct
+   return hs.fnutils.imap(unitRects, function(unitRect)
+      return hs.geometry.fromUnitRect(unitRect, screenFrame)
+   end)
+end
+
+-- Resize and center windows ---------------------------------------------
+--------------------------------------------------------------------------
+M.resizeAndCenter = function(fraction)
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   local screenMax = win:screen():frame()
+
+   f.w = screenMax.w * fraction
+   f.h = screenMax.h * fraction
+   f.x = screenMax.x + (screenMax.w - f.w) / 2
+   f.y = screenMax.y + (screenMax.h - f.h) / 2
+
+   setFrame(win, f, 0)
+end
+
+return M
--- hammerspoon/.hammerspoon/window_management.lua
+++ hammerspoon/.hammerspoon/window_management.lua
@@ -1,128 +1,14 @@
 local constants = require("common/constants")
 local hyperkey = constants.hyperkey
 
--- https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-1294971600
--- Some applications (eg Firefox) animate resize instead of moving, due to AXEnhancedUserInterface
--- So, temporarily disable it while moving the window
-local function axHotfix(win)
-   if not win then
-      win = hs.window.frontmostWindow()
-   end
+local utils = require("window_management-utils")
+local cyclePositions = utils.cyclePositions
+local withAxHotfix = utils.withAxHotfix
+local mapScreenUnitRects = utils.mapScreenUnitRects
+local resizeAndCenter = utils.resizeAndCenter
 
-   local axApp = hs.axuielement.applicationElement(win:application())
-   if not axApp then
-      return
-   end
-   local wasEnhanced = axApp.AXEnhancedUserInterface
-   axApp.AXEnhancedUserInterface = false
-
-   return function()
-      hs.timer.doAfter(hs.window.animationDuration * 2, function()
-         axApp.AXEnhancedUserInterface = wasEnhanced
-      end)
-   end
-end
-
--- TODO?: add branching logic for firefox, for potential performance
-local function withAxHotfix(fn, position)
-   if not position then
-      position = 1
-   end
-   local revert = axHotfix()
-   fn()
-   if revert then
-      revert()
-   end
-end
-
-local MARGIN = 4
--- Buffer margin space around screen edges
-local function applyScreenEdgeMargins(window, screen)
-   local x, y, width, height = window.x, window.y, window.w, window.h
-   -- left edge
-   if x - screen.x < MARGIN then
-      width = width - MARGIN
-      x = screen.x + MARGIN
-   end
-   -- right edge
-   if screen.w - (x + width) < MARGIN then
-      width = width - MARGIN
-   end
-   -- top edge
-   if y - screen.y < MARGIN then
-      height = height - MARGIN
-      y = screen.y + MARGIN
-   end
-   -- bottom edge
-   if screen.h - (y + height) < MARGIN then
-      height = height - MARGIN
-   end
-   return { x = x, y = y, w = width, h = height }
-end
-
-local function withinMargin(a, b)
-   return math.abs(a - b) <= MARGIN * 2
-end
-
----@param win hs.window
----@param rect hs.geometry
----@param duration number
-local function setFrame(win, rect, duration)
-   local newRect = applyScreenEdgeMargins(rect, win:screen():frame())
-   withAxHotfix(function()
-      win:setFrame(newRect, duration)
-   end)
-end
-
----Cycles through a list of rects to set focused window position
----@param options {h: number, w: number, x: number, y: number}[] list of rect to cycle through
-local function cyclePositions(options)
-   local win = hs.window.focusedWindow()
-   local frame = win:frame()
-
-   local function calculateDuration(appName, sameSize, sameHeight)
-      if appName == "Acrobat Reader" then
-         return 0
-      end
-      return sameSize and 0.15 or sameHeight and 0.1 or 0
-   end
-
-   local function matchesOption(rect, option)
-      return withinMargin(rect.h, option.h)
-         and withinMargin(rect.w, option.w)
-         and withinMargin(rect.x, option.x)
-         and withinMargin(rect.y, option.y)
-   end
-
-   local firstOption = options[1]
-   local nextOption = firstOption
-
-   for i, option in ipairs(options) do
-      if matchesOption(frame, option) then
-         nextOption = options[i + 1] or firstOption
-         break
-      end
-   end
-
-   local sameSize = withinMargin(frame.h, nextOption.h) and withinMargin(frame.w, nextOption.w)
-   local sameHeight = withinMargin(frame.h, nextOption.h)
-   local duration = calculateDuration(win:application():name(), sameSize, sameHeight)
-
-   setFrame(win, nextOption, duration)
-end
-
----Converts a list of unit rects to screen rects
----@param unitRects {x: number, y: number, w: number, h: number}[]
----@return hs.geometry
-local function mapScreenUnitRects(unitRects)
-   local win = hs.window.focusedWindow()
-   local screenFrame = win:screen():frame()
-
-   ---@diagnostic disable-next-line: return-type-mismatch it's correct
-   return hs.fnutils.imap(unitRects, function(unitRect)
-      return hs.geometry.fromUnitRect(unitRect, screenFrame)
-   end)
-end
+-- Move to position --------------------------
+----------------------------------------------
 
 -- Left positions
 hs.hotkey.bind(hyperkey, "H", function()
@@ -180,44 +66,8 @@ hs.hotkey.bind(hyperkey, "C", function()
     cyclePositions(mapScreenUnitRects(unitRects))
 end)
 
-local function centerThin()
-   local win = hs.window.focusedWindow()
-   if not win then
-      hs.alert("No focused window")
-      return
-   end
-   local screenFrame = win:screen():frame()
-   local widthFraction = 0.2
-
-   cyclePositions({
-      {
-         x = screenFrame.x + (screenFrame.w * (1 - widthFraction) / 2),
-         y = screenFrame.y,
-         w = screenFrame.w * widthFraction,
-         h = screenFrame.h,
-      },
-   })
-end
-
--- FIXME: calling this form raycast results in win being nil
-hs.urlevent.bind("centerThin", function()
-   hs.timer.doAfter(1, centerThin)
-end)
-
--- Resize and center windows ---------------------------------------------
---------------------------------------------------------------------------
-local function resizeAndCenter(fraction)
-   local win = hs.window.focusedWindow()
-   local f = win:frame()
-   local screenMax = win:screen():frame()
-
-   f.w = screenMax.w * fraction
-   f.h = screenMax.h * fraction
-   f.x = screenMax.x + (screenMax.w - f.w) / 2
-   f.y = screenMax.y + (screenMax.h - f.h) / 2
-
-   setFrame(win, f, 0)
-end
+-- Resize and center windows --------------------
+-------------------------------------------------
 
 -- Cycle through window sizes based on current position
 hs.hotkey.bind(hyperkey, "M", function()
@@ -239,26 +89,21 @@ hs.hotkey.bind(hyperkey, "M", function()
    resizeAndCenter(sizes[nextSizeIndex])
 end)
 
--- Move to display -------------------------------------------------------
---------------------------------------------------------------------------
--- Move to the right screen
--- TODO: maintain relative size after moving
--- tried refactoring with hs.window:moveToScreen -- this doesn't wrap
+-- Move to display -------------------------------
+--------------------------------------------------
+
+-- To the right
+-- NOTE: hs.window:moveToScreen doesn't wrap
 hs.hotkey.bind({ "ctrl", "shift", "cmd" }, "L", function()
    local win = hs.window.focusedWindow()
    local screen = win:screen()
-   -- local app = win:application()
 
    withAxHotfix(function()
-      -- two methods for this
-      -- win:moveOneScreenEast(false, nil, 0) -- does not wrap
       win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
    end)
-
-   -- Another method
 end)
 
--- Move to the left screen
+-- To the left
 hs.hotkey.bind({ "ctrl", "shift", "cmd" }, "H", function()
    local win = hs.window.focusedWindow()
    local screen = win:screen()
