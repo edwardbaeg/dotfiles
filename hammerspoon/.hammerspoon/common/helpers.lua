eec421f7097eb2a766bb5419f7fcb007a7fa4779
edwardbaeg <edwardbaeg9@gmail.com>
2025-05-07 23:00:59 -0400

    Refactor: remove unnecessary

--- hammerspoon/.hammerspoon/common/helpers.lua
+++ hammerspoon/.hammerspoon/common/helpers.lua
@@ -1,23 +1,23 @@
-local module = {}
+local M = {}
 
 -- functions for remapping key combos with press and hold behavior
-function module.getPressAndHoldFn(sendModifiers, key)
+local function getPressAndHoldFn(sendModifiers, key)
    return function()
       hs.eventtap.keyStroke(sendModifiers, key, 1000)
    end
 end
 
-function module.sendKey(modifiers, key, sendModifiers, sendKey)
+function M.sendKey(modifiers, key, sendModifiers, sendKey)
    hs.hotkey.bind(
       modifiers,
       key,
-      module.getPressAndHoldFn(sendModifiers, sendKey),
+      getPressAndHoldFn(sendModifiers, sendKey),
       nil,
-      module.getPressAndHoldFn(sendModifiers, sendKey)
+      getPressAndHoldFn(sendModifiers, sendKey)
    )
 end
 
-function module.sendSystemKey(modifiers, key, sendKey)
+function M.sendSystemKey(modifiers, key, sendKey)
    hs.hotkey.bind(modifiers, key, function()
       hs.eventtap.event.newSystemKeyEvent(sendKey, true):post()
       hs.eventtap.event.newSystemKeyEvent(sendKey, false):post()
@@ -28,11 +28,4 @@ hs.hotkey.bind({ "cmd" }, "M", function()
    hs.alert("[ cmd + m ] disabled") -- minimizes the current window. Annoying because does not unminimize on focus
 end)
 
--- NOTE: This doesn't seem to work with window-management, "cannot get focused window"
--- openRaycastDeeplink("raycast://extensions/raycast/window-management/move-right")
--- openRaycastDeeplink("raycast://extensions/raycast/raycast/confetti")
-function module.openDeeplink(deeplink)
-    hs.execute("open '" .. deeplink .. "'")
-end
-
-return module
+return M
