7b9397269e1269ba1eee763284f1c2968bafadab
edwardbaeg <edwardbaeg9@gmail.com>
2025-05-20 12:48:36 -0400

    Feat: add codecompanion

---
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,17 @@
+return {
+   "olimorris/codecompanion.nvim",
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+   },
+   opts = {
+      strategies = {
+         chat = {
+            adapter = "copilot",
+         },
+         inline = {
+            adapter = "copilot",
+         }
+      }
+   },
+}
