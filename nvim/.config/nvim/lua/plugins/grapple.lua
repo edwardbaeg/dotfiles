bf0c234e3cc6ba5bd94f763dad84aa8adf08013d
edwardbaeg <edwardbaeg9@gmail.com>
2025-05-21 17:13:22 -0400

    Disable unused mappings

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -53,14 +53,14 @@ return {
 
       -- Quick jump to tag
       -- this is not really used
-      { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple select 1" },
-      { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple select 2" },
-      { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple select 3" },
-      { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple select 4" },
-      { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple select 5" },
-      { "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple select 6" },
-      { "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple select 7" },
-      { "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple select 8" },
-      { "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple select 9" },
+      -- { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple select 1" },
+      -- { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple select 2" },
+      -- { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple select 3" },
+      -- { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple select 4" },
+      -- { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple select 5" },
+      -- { "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple select 6" },
+      -- { "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple select 7" },
+      -- { "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple select 8" },
+      -- { "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple select 9" },
    },
 }
