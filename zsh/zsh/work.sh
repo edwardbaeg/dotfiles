1e249f4254e77c57a80e165c3fb57fc52f799fa8
edwardbaeg <edwardbaeg9@gmail.com>
2025-05-22 11:29:47 -0400

    Feat: convert work zsh module to bash

--- zsh/zsh/work.sh
+++ zsh/zsh/work.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env bash
+
+# -- Aliases
 alias gcm2="git checkout master2 && git reset --hard origin/master"
 alias front="cd ~/dev/oneadvisory/frontend"
 alias work="cd ~/dev/oneadvisory/frontend-worktree"
 alias back="cd ~/dev/oneadvisory/backend"
 alias decrypt="cd ~/dev/oneadvisory/backend/ && ./bin/dispatch crypto decrypt" # TODO?: create a function for this to add quotes and return to previous directory
 
-# -- TODO: migrate to .env
+# -- TODO?: migrate to .env and setup direnv?
 export AWS_PROFILE=aws-dispatch-dev01 # use this for forms building
 # export AWS_PROFILE=oa-prod # use this for forms building
 # export AWS_PROFILE=dispatch-dev01  # use this for development
@@ -13,6 +16,5 @@ export AWS_PROFILE=aws-dispatch-dev01 # use this for forms building
 export AWS_REGION=us-east-1
 export ECR_AWS_PROFILE=oa-dev
 
-# run dc locally
+# to run dc locally
 # AWS_PROFILE=dispatch-dev01 CHAMBER_TENANT=develop/ npx yarn dev:services
-
