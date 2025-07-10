25f515928cd050832be19852d7df09da6807c94f
edwardbaeg <edwardbaeg9@gmail.com>
2025-07-10 14:56:32 -0400

    Feat: separate prod/default decrypt commands

--- zsh/zsh/work.sh
+++ zsh/zsh/work.sh
@@ -7,7 +7,7 @@ alias work="cd ~/dev/oneadvisory/frontend-worktree"
 alias back="cd ~/dev/oneadvisory/backend"
 
 # decrypts argument or clipboard, outputs result and copies to clipboard
-decrypt() {
+decrypt-prod() {
 	pushd ~/dev/oneadvisory/backend/ || return 1
 	export AWS_PROFILE=oa-prod # aws profile with `prod` acccess
 	ensure_sso
@@ -15,6 +15,22 @@ decrypt() {
 	export CRYPTO_PRIMARY_KEY_SECRET
 	CRYPTO_PRIMARY_KEY_ID=$(chamber read -q prod/auth CRYPTO_PRIMARY_KEY_ID)
 	export CRYPTO_PRIMARY_KEY_ID
+	if [ $# -eq 0 ]; then
+		./bin/dispatch crypto decrypt "$(pbpaste)" | jq | tee >(pbcopy)
+	else
+		./bin/dispatch crypto decrypt "$@" | jq | tee >(pbcopy)
+	fi
+	popd || return
+}
+
+decrypt-default() {
+	pushd ~/dev/oneadvisory/backend/ || return 1
+	# export AWS_PROFILE=oa-prod # aws profile with `prod` acccess
+	# ensure_sso
+	# CRYPTO_PRIMARY_KEY_SECRET=$(chamber read -q prod/auth CRYPTO_PRIMARY_KEY_SECRET)
+	# export CRYPTO_PRIMARY_KEY_SECRET
+	# CRYPTO_PRIMARY_KEY_ID=$(chamber read -q prod/auth CRYPTO_PRIMARY_KEY_ID)
+	# export CRYPTO_PRIMARY_KEY_ID
 	if [ $# -eq 0 ]; then
 		./bin/dispatch crypto decrypt "$(pbpaste)" | jq | tee >(pbcopy)
 	else
