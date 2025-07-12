abca903d4b1a5baadca669fa895767f14cfc3208
edwardbaeg <edwardbaeg9@gmail.com>
2025-07-11 23:33:42 -0400

    Feat: add c-w to exit terminal insert

--- nvim/.config/nvim/lua/plugins/collection.snacks.lua
+++ nvim/.config/nvim/lua/plugins/collection.snacks.lua
@@ -148,15 +148,31 @@ return {
          ---@diagnostic disable-next-line: missing-fields
          terminal = {
             keys = {
-               -- term_normal = {
-               --    "<c-w>",
-               --    function()
-               --       vim.cmd("stopinsert")
-               --    end,
-               --    mode = "t",
-               --    expr = true,
-               --    desc = "Exit terminal mode to normal mode",
-               -- },
+               {
+                  "<c-w>",
+                  function()
+                     vim.cmd("stopinsert")
+                  end,
+                  mode = "t",
+                  expr = true,
+                  desc = "Exit terminal mode to normal mode",
+               },
+               term_normal = {
+                  "<esc>",
+                  function(self)
+                     self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+                     if self.esc_timer:is_active() then
+                        self.esc_timer:stop()
+                        vim.cmd("stopinsert")
+                     else
+                        self.esc_timer:start(200, 0, function() end)
+                        return "<esc>"
+                     end
+                  end,
+                  mode = "t",
+                  expr = true,
+                  desc = "Double escape to normal mode",
+               },
             },
          },
       },
