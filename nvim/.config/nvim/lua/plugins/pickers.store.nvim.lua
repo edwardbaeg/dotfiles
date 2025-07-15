4de7b6023a6086b41005b9e4151fab9de6a4892c
edwardbaeg <edwardbaeg9@gmail.com>
2025-07-15 09:59:51 -0400

    Feat: add Store plugin

---
+++ nvim/.config/nvim/lua/plugins/pickers.store.nvim.lua
@@ -0,0 +1,11 @@
+return {
+   "alex-popov-tech/store.nvim",
+   dependencies = {
+      "OXY2DEV/markview.nvim", -- optional, for pretty readme preview / help window
+   },
+   cmd = "Store",
+   -- keys = {
+   --    { "<leader>s", "<cmd>Store<cr>", desc = "Open Plugin Store" },
+   -- },
+   opts = {},
+}
