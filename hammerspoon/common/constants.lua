8de65b83d18d27a82c832a4782bea6d0b5ea2bf5
edwardbaeg <edwardbaeg9@gmail.com>
2025-07-30 15:20:56 -0400

    Add color typings

--- hammerspoon/common/constants.lua
+++ hammerspoon/common/constants.lua
@@ -1,5 +1,18 @@
 ---@alias ModifierKeys string[] Array of modifier key names
 
+---@class Color
+---@field red number Red component (0-1)
+---@field green number Green component (0-1)
+---@field blue number Blue component (0-1)
+---@field alpha number Alpha component (0-1)
+
+---@class Colors
+---@field orange Color
+---@field grey Color
+---@field lightBlue Color
+---@field purple Color
+---@field navy Color
+
 ---@class Constants
 ---@field hyperkey ModifierKeys Modifier keys for hyperkey (cmd + ctrl)
 ---@field cmdShift ModifierKeys Modifier keys for cmd + shift
@@ -7,6 +20,7 @@
 ---@field allkey ModifierKeys Modifier keys for cmd + ctrl + shift
 ---@field isPersonal boolean Whether this is running on personal machine
 ---@field screenPadding number Screen edge padding in pixels
+---@field colors Colors Color definitions
 local M = {}
 
 M.hyperkey = { "cmd", "ctrl" }
