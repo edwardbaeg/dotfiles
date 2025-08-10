0cc06f7a7670125a5ca547ecfc36094c179c8d30
edwardbaeg <edwardbaeg9@gmail.com>
2025-08-09 21:33:17 -0400

    Feat: update colorful-winsep with animations

--- nvim/lua/plugins/visual.colorful-winsep.lua
+++ nvim/lua/plugins/visual.colorful-winsep.lua
@@ -2,34 +2,21 @@ return {
    -- color the line separating windows
    "nvim-zh/colorful-winsep.nvim",
    enabled = not vim.g.vscode,
-   branch = "alpha",
    event = "VeryLazy",
-   -- config = true,
 
-   -- TODO: decrease border width
    config = function()
       -- local frappe = require("catppuccin.palettes").get_palette("frappe")
       require("colorful-winsep").setup({
-         -- events = { "WinEnter", "WinResized" },
-         hi = {
-            --    bg = "none",
-            -- fg = "cyan4",
+         animate = {
+            enabled = 'shift', -- default is false
+            shift = {
+               delta_time = 0.1,
+               smooth_speed = 1,
+               delay = 3,
+            },
          },
-         --
-         -- integrations = {
-         --    bufferline = true,
-         -- },
-         -- interval = 1000,
-
-         -- These aren't working...
-         only_line_seq = false,
-         smooth = true,
-         exponential_smoothing = true,
-         anchor = {
-            left = { height = 1, x = -1, y = -1 },
-            right = { height = 1, x = -1, y = 0 },
-            up = { width = 0, x = -1, y = 0 },
-            bottom = { width = 0, x = 1, y = 0 },
+         indicator_for_2wins = {
+            position = "both", -- default is nil
          },
       })
    end,
