7ae549d8b059deba4aca14be32c5df6bd411eab6
edwardbaeg <edwardbaeg9@gmail.com>
2025-08-27 10:34:02 -0400

    Fix: properly loading dart sessions

--- nvim/lua/plugins/sessions.lua
+++ nvim/lua/plugins/sessions.lua
@@ -18,17 +18,18 @@ return {
          end, { desc = "[s]ession select [f]uzzy", silent = true })
 
          -- Dart session integration
+         -- TODO; would be nice if this was tied to the specific session, but using persistence.current() to get the filename does not work...
          vim.api.nvim_create_autocmd("User", {
             pattern = "PersistenceLoadPost",
             callback = function()
-               require("dart").read_session("test")
+               require("dart").read_session('dart')
             end,
          })
 
          vim.api.nvim_create_autocmd("User", {
-            pattern = "PersistenceSavePre",
+            pattern = "PersistenceSavePost",
             callback = function()
-               require("dart").write_session("test")
+               require("dart").write_session('dart')
             end,
          })
       end,
