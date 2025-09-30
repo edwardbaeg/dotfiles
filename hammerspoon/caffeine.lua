4c077a33ba0d98cd541e9ac44c2cd681287ec5fa
edwardbaeg <35383701+edwardbaeg@users.noreply.github.com>
2025-09-30 12:04:08 -0400

    Feat: add toggle feature abstraction

--- hammerspoon/caffeine.lua
+++ hammerspoon/caffeine.lua
@@ -1,59 +1,39 @@
 ---@type Constants
 local constants = require("common/constants")
 local isPersonal = constants.isPersonal
+local toggleFeature = require("common/toggle_feature")
 
 -- Caffeine menubar app --------------------------------------------------
 --------------------------------------------------------------------------
 -- Caffeine state can be toggled by clicking on the menu bar
--- or by linking to `hammerspoon://toggleCaffineState?state={true|false}`
-
--- Settings key for persisting caffeine state
-local CAFFEINE_STATE_KEY = "caffeineState"
-
--- Create menubar item for tracking caffeinated state
--- TODO: persist state using hs.settings
--- https://www.hammerspoon.org/docs/hs.settings.html
-local caffeineMenuBar = hs.menubar.new()
-
-local caffeine = {}
-
-local function setCaffeineDisplay(state)
-   if caffeineMenuBar then
-      if state then
-         caffeineMenuBar:setTitle("A") -- for awake
-      else
-         caffeineMenuBar:setTitle("S") -- for sleep
-      end
-      -- Save state to settings
-      hs.settings.set(CAFFEINE_STATE_KEY, state)
-   end
-end
+-- or by using URLs:
+-- hammerspoon://caffeine?action=enable
+-- hammerspoon://caffeine?action=disable
+-- hammerspoon://caffeine?action=toggle (or just hammerspoon://caffeine)
 
 local sleepType = "displayIdle"
 
-function caffeine.enable()
-   hs.caffeinate.set(sleepType, true)
-   setCaffeineDisplay(true)
-end
+-- Create caffeine toggle feature using the common abstraction
+local caffeine = toggleFeature.new({
+   name = "caffeine",
+   settingsKey = "caffeineState",
+   menubar = {
+      enabledTitle = "A", -- for awake
+      disabledTitle = "S" -- for sleep
+   },
+   onEnable = function()
+      hs.caffeinate.set(sleepType, true)
+   end,
+   onDisable = function()
+      hs.caffeinate.set(sleepType, false)
+   end,
+   defaultState = false
+})
 
-function caffeine.disable()
-   hs.caffeinate.set(sleepType, false)
-   setCaffeineDisplay(false)
-end
-
-function caffeine.toggle()
-   local currentState = hs.caffeinate.get(sleepType)
-   local newState = not currentState
-   if newState then
-      caffeine.enable()
-   else
-      caffeine.disable()
-   end
-end
-
----@diagnostic disable-next-line: unused-local _eventName is unused
-local handleCaffeineUrl = function(_eventName, params)
-   local state = params["state"]
+-- Legacy URL support for backward compatibility
+-- Usage: hammerspoon://toggleCaffeineState?state={true|false}
+hs.urlevent.bind("toggleCaffeineState", function(_eventName, params)
+   local state = params and params["state"]
    if state then
       if state == "true" then
          caffeine.enable()
@@ -65,29 +45,9 @@ local handleCaffeineUrl = function(_eventName, params)
    else
       caffeine.toggle()
    end
-end
+end)
 
-local function caffeineClicked()
-   caffeine.toggle()
-end
-
-if caffeineMenuBar then
-   caffeineMenuBar:setClickCallback(caffeineClicked)
-   -- Initialize state from settings, defaulting to current system state if not set
-   local savedState = hs.settings.get(CAFFEINE_STATE_KEY)
-   if savedState ~= nil then
-      if savedState then
-         caffeine.enable()
-      else
-         caffeine.disable()
-      end
-   else
-      setCaffeineDisplay(hs.caffeinate.get(sleepType))
-   end
-end
-
--- Usage: hammerspoon://toggleCaffeineState?state={true|false}
-hs.urlevent.bind("toggleCaffeineState", handleCaffeineUrl)
+-- Legacy URL support
 hs.urlevent.bind("enableCaffeine", caffeine.enable)
 hs.urlevent.bind("disableCaffeine", caffeine.disable)
 
@@ -111,6 +71,18 @@ if isPersonal then
    -- NOTE: do not create watcher as local variable or it will be garbage collected
    RyujinxWatcher = hs.application.watcher.new(watchRyujinx)
    RyujinxWatcher:start()
+end
+
+-- Initialize based on current caffeinate state if no saved setting exists
+local savedState = hs.settings.get("caffeineState")
+if savedState == nil then
+   -- Sync with current system state on first run
+   local currentSystemState = hs.caffeinate.get(sleepType)
+   if currentSystemState then
+      caffeine.enable()
+   else
+      caffeine.disable()
+   end
 end
 
 return caffeine
---
+++ hammerspoon/common/toggle_feature.lua
@@ -0,0 +1,106 @@
+-- Toggle Feature Abstraction Module ----------------------------------------
+--------------------------------------------------------------------------
+-- Provides common functionality for creating toggleable features with:
+-- - Persistent settings using hs.settings
+-- - Menubar item with state display
+-- - URL event handlers with parameter-based actions
+-- - Enable/disable/toggle functions
+
+local M = {}
+
+-- Creates a new toggle feature instance
+-- @param config table Configuration object with the following fields:
+--   - name: string - Used for URL event binding (e.g., "autoReload")
+--   - settingsKey: string - Key for hs.settings persistence
+--   - menubar: table - Menubar configuration with:
+--     - enabledTitle: string - Title shown when enabled
+--     - disabledTitle: string - Title shown when disabled
+--   - onEnable: function - Called when feature is enabled
+--   - onDisable: function - Called when feature is disabled
+--   - defaultState: boolean - Default state if no saved setting (default: false)
+-- @return table - Toggle feature instance
+function M.new(config)
+    -- Validate required config
+    assert(config.name, "config.name is required")
+    assert(config.settingsKey, "config.settingsKey is required")
+    assert(config.menubar, "config.menubar is required")
+    assert(config.menubar.enabledTitle, "config.menubar.enabledTitle is required")
+    assert(config.menubar.disabledTitle, "config.menubar.disabledTitle is required")
+    assert(config.onEnable, "config.onEnable is required")
+    assert(config.onDisable, "config.onDisable is required")
+
+    local instance = {
+        config = config,
+        menubar = hs.menubar.new()
+    }
+
+    -- Check if feature is currently enabled
+    function instance.isEnabled()
+        local state = hs.settings.get(config.settingsKey)
+        return state == nil and (config.defaultState or false) or state
+    end
+
+    -- Update menubar display and save state
+    local function setDisplay(state)
+        if instance.menubar then
+            local title = state and config.menubar.enabledTitle or config.menubar.disabledTitle
+            instance.menubar:setTitle(title)
+            -- Save state to settings
+            hs.settings.set(config.settingsKey, state)
+        end
+    end
+
+    -- Enable the feature
+    function instance.enable()
+        setDisplay(true)
+        config.onEnable()
+    end
+
+    -- Disable the feature
+    function instance.disable()
+        setDisplay(false)
+        config.onDisable()
+    end
+
+    -- Toggle the feature state
+    function instance.toggle()
+        if instance.isEnabled() then
+            instance.disable()
+        else
+            instance.enable()
+        end
+    end
+
+    -- Get menubar instance (for advanced customization if needed)
+    function instance.getMenubar()
+        return instance.menubar
+    end
+
+    -- Set up menubar click callback
+    if instance.menubar then
+        instance.menubar:setClickCallback(instance.toggle)
+    end
+
+    -- Set up URL event handler with parameter-based actions
+    hs.urlevent.bind(config.name, function(_eventName, params)
+        local action = params and params.action or "toggle"
+
+        if action == "enable" then
+            instance.enable()
+        elseif action == "disable" then
+            instance.disable()
+        elseif action == "toggle" then
+            instance.toggle()
+        else
+            hs.alert("Invalid action: " .. tostring(action) .. ". Use enable, disable, or toggle.")
+        end
+    end)
+
+    -- Initialize from saved state
+    local savedState = instance.isEnabled()
+    setDisplay(savedState)
+
+    return instance
+end
+
+return M
