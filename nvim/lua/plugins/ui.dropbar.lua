3400eaf94573fb177f5eafc98ad06c24885e0890
edwardbaeg <35383701+edwardbaeg@users.noreply.github.com>
2025-10-20 16:44:34 -0400

    Revert showing filename

--- nvim/lua/plugins/ui.dropbar.lua
+++ nvim/lua/plugins/ui.dropbar.lua
@@ -7,31 +7,20 @@ return {
    --    "nvim-telescope/telescope-fzf-native.nvim",
    --    build = "make",
    -- },
-   config = function()
-      local sources = require("dropbar.sources")
-      local utils = require("dropbar.utils")
-
-      local custom_path = {
-         get_symbols = function(_buf, _win, _cursor)
-            return {} -- Remove all path symbols to show only code breadcrumbs
-         end,
+   -- config = function()
+   --    local dropbar_api = require("dropbar.api")
+   --    vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+   --    vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+   --    vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+   -- end,
+   -- config = true,
+   opts = {
+      sources = {
+         path = {
+            filter = function(_filename)
+               return true
+            end
+         }
       }
-
-      require("dropbar").setup({
-         bar = {
-            sources = function(buf, _)
-               if vim.bo[buf].ft == "markdown" then
-                  return { custom_path, sources.markdown }
-               end
-               if vim.bo[buf].buftype == "terminal" then
-                  return { sources.terminal }
-               end
-               return {
-                  custom_path,
-                  utils.source.fallback({ sources.lsp, sources.treesitter }),
-               }
-            end,
-         },
-      })
-end,
+   },
 }
