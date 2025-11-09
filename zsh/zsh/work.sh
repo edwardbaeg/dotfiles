cb41b122e63b7530fb080f48f4c615e0b96ac92c
edwardbaeg <35383701+edwardbaeg@users.noreply.github.com>
2025-11-08 22:23:07 -0500

    Flip aws env

--- zsh/zsh/work.sh
+++ zsh/zsh/work.sh
@@ -47,9 +47,9 @@ ensure_sso() {
 }
 
 # -- TODO?: migrate to .env and setup direnv?
-export AWS_PROFILE=aws-dispatch-dev01 # use this for forms building
+# export AWS_PROFILE=aws-dispatch-dev01 # use this for forms building
 # export AWS_PROFILE=oa-prod # use this for forms building # use this for decrypt
-# export AWS_PROFILE=dispatch-dev01  # use this for development
+export AWS_PROFILE=dispatch-dev01  # use this for development
 # export AWS_PROFILE=oa-dev
 # export AWS_PROFILE=duplo
 export AWS_REGION=us-east-1
