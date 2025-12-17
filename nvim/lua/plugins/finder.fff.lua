303c6b40a0a39307efec6482b8a62ca6be6306ad
edwardbaeg <35383701+edwardbaeg@users.noreply.github.com>
2025-12-17 13:37:40 -0500

    Update build process

--- nvim/lua/plugins/finder.fff.lua
+++ nvim/lua/plugins/finder.fff.lua
@@ -1,7 +1,18 @@
 return {
    "dmtrKovalenko/fff.nvim",
-   build = "cargo build --release",
+   lazy = false, -- plugin automatically lazy loads
+   -- if build fails, navigate to plugin dir and run `cargo build --release`
+   -- build = "cargo build --release",
+   build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
+   end,
    opts = {
+      debug = {
+         enabled = true,     -- we expect your collaboration at least during the beta
+         show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+      },
       prompt = " ▶ ", -- Input prompt symbol
       keymaps = {
          close = { "<Esc>", "<c-c>" },
@@ -19,7 +30,7 @@ return {
          desc = "Toggle FFF",
       },
       {
-         "<c-p>", -- try it if you didn't it is a banger keybinding for a picker
+         "<c-p>",
          function()
             require("fff").find_files()
          end,
