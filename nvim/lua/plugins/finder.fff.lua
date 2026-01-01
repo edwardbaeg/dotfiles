b3ce03df0e633c9cac2f77a51ca782e9329f2158
edwardbaeg <35383701+edwardbaeg@users.noreply.github.com>
2026-01-01 14:56:23 -0500

    Update comment

--- nvim/lua/plugins/finder.fff.lua
+++ nvim/lua/plugins/finder.fff.lua
@@ -1,7 +1,7 @@
 return {
    "dmtrKovalenko/fff.nvim",
    lazy = false, -- plugin automatically lazy loads
-   -- if build fails, navigate to plugin dir and run `cargo build --release`
+   -- if build fails, navigate to plugin dir and run `cargo build --release` (.local/share/nvim/lazy/fff)
    -- build = "cargo build --release",
    build = function()
       -- this will download prebuild binary or try to use existing rustup toolchain to build from source
