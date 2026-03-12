7c9d8f0682c8abea5d88d9fefd9191cb4b1f84a0
ed <35383701+edwardbaeg@users.noreply.github.com>
2026-03-12 17:43:05 -0400

    Feat: add application check

--- raycast/_enabled-commands/configure-zsa-oryx.sh
+++ raycast/_enabled-commands/configure-zsa-oryx.sh
@@ -13,8 +13,16 @@
 # @raycast.author dwrdbg
 # @raycast.authorURL https://raycast.com/dwrdbg
 
+open_browser() {
+	if open -Ra "Arc" 2>/dev/null; then
+		open -a "Arc" "$1"
+	else
+		open "$1"
+	fi
+}
+
 if [ "$1" = "voyager" ]; then
-	open -a "Arc" "https://configure.zsa.io/voyager/layouts/Bq47A/latest/0"
+	open_browser "https://configure.zsa.io/voyager/layouts/Bq47A/latest/0"
 elif [ "$1" = "moonlander" ]; then
-	open -a "Arc" "https://configure.zsa.io/moonlander/layouts/blnlP/latest/0"
+	open_browser "https://configure.zsa.io/moonlander/layouts/blnlP/latest/0"
 fi
