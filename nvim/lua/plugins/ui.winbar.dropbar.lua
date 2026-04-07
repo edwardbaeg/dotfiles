ab2ca62e7e6a7777fb08c29fc29ff75d3a2256df
ed <35383701+edwardbaeg@users.noreply.github.com>
2026-04-07 15:49:51 -0400

    Feat: remove lua table comments from winbar

--- nvim/lua/plugins/ui.winbar.dropbar.lua
+++ nvim/lua/plugins/ui.winbar.dropbar.lua
@@ -2,11 +2,6 @@ return {
    -- IDE style winbar with breadcrumbs
    -- drop in; doesn't require any configuration
    "Bekaboo/dropbar.nvim",
-   -- optional, but required for fuzzy finder support
-   -- dependencies = {
-   --    "nvim-telescope/telescope-fzf-native.nvim",
-   --    build = "make",
-   -- },
    -- config = function()
    --    local dropbar_api = require("dropbar.api")
    --    vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
@@ -14,13 +9,21 @@ return {
    --    vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
    -- end,
    -- config = true,
-   opts = {
-      sources = {
-         path = {
-            filter = function(_filename)
-               return true
-            end,
-         },
-      },
-   },
+   opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.path = {
+         filter = function(_filename)
+            return true
+         end,
+      }
+      -- Remove 'table' to prevent comment text inside tables from appearing as crumb names
+      -- (mostly affects lua-family languages and toml; safe to remove for js/ts/py/go etc.)
+      local defaults = require("dropbar.configs").opts.sources.treesitter.valid_types
+      opts.sources.treesitter = vim.tbl_extend("force", opts.sources.treesitter or {}, {
+         valid_types = vim.tbl_filter(function(t)
+            return t ~= "table"
+         end, defaults),
+      })
+      return opts
+   end,
 }
