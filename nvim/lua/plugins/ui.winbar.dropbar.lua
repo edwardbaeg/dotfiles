c74ffd23645fbd2648e3b23140e88458d0b4a691
ed <35383701+edwardbaeg@users.noreply.github.com>
2026-04-07 15:57:45 -0400

    Feat: disable click and hover handlers

--- nvim/lua/plugins/ui.winbar.dropbar.lua
+++ nvim/lua/plugins/ui.winbar.dropbar.lua
@@ -2,20 +2,9 @@ return {
    -- IDE style winbar with breadcrumbs
    -- drop in; doesn't require any configuration
    "Bekaboo/dropbar.nvim",
-   -- config = function()
-   --    local dropbar_api = require("dropbar.api")
-   --    vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-   --    vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-   --    vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-   -- end,
-   -- config = true,
    opts = function(_, opts)
       opts.sources = opts.sources or {}
-      opts.sources.path = {
-         filter = function(_filename)
-            return true
-         end,
-      }
+
       -- Remove 'table' to prevent comment text inside tables from appearing as crumb names
       -- (mostly affects lua-family languages and toml; safe to remove for js/ts/py/go etc.)
       local defaults = require("dropbar.configs").opts.sources.treesitter.valid_types
@@ -24,6 +13,9 @@ return {
             return t ~= "table"
          end, defaults),
       })
+      -- Disable click/menu functionality for performance
+      opts.bar = vim.tbl_extend("force", opts.bar or {}, { hover = false })
+      opts.symbol = vim.tbl_extend("force", opts.symbol or {}, { on_click = false })
       return opts
    end,
 }
