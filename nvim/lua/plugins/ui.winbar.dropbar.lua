0fd5451019224be2453a2a8c313c973f4e0412e1
ed <35383701+edwardbaeg@users.noreply.github.com>
2026-04-09 22:13:50 -0400

    Refactor: use lazy opts field

--- nvim/lua/plugins/ui.winbar.dropbar.lua
+++ nvim/lua/plugins/ui.winbar.dropbar.lua
@@ -2,20 +2,25 @@ return {
    -- IDE style winbar with breadcrumbs
    -- drop in; doesn't require any configuration
    "Bekaboo/dropbar.nvim",
-   opts = function(_, opts)
-      opts.sources = opts.sources or {}
-
+   opts = {
+      bar = {
+         hover = false,
+      },
+      symbol = {
+         on_click = false,
+      },
+   },
+   config = function(_, opts)
       -- Remove 'table' to prevent comment text inside tables from appearing as crumb names
       -- (mostly affects lua-family languages and toml; safe to remove for js/ts/py/go etc.)
       local defaults = require("dropbar.configs").opts.sources.treesitter.valid_types
-      opts.sources.treesitter = vim.tbl_extend("force", opts.sources.treesitter or {}, {
-         valid_types = vim.tbl_filter(function(t)
-            return t ~= "table"
-         end, defaults),
-      })
-      -- Disable click/menu functionality for performance
-      opts.bar = vim.tbl_extend("force", opts.bar or {}, { hover = false })
-      opts.symbol = vim.tbl_extend("force", opts.symbol or {}, { on_click = false })
-      return opts
+      opts.sources = {
+         treesitter = {
+            valid_types = vim.tbl_filter(function(t)
+               return t ~= "table"
+            end, defaults),
+         },
+      }
+      require("dropbar").setup(opts)
    end,
 }
