bdba056980d6ddcd30ae8a88b55305f82d2032f0
ed <35383701+edwardbaeg@users.noreply.github.com>
2026-06-25 16:59:00 -0400

    feat: add max all display brightness script

---
+++ raycast/_enabled-commands/max-brightness-all-displays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Max Brightness All Displays
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ☀️
+# @raycast.author dwrdbg
+# @raycast.authorURL https://raycast.com/dwrdbg
+
+PATH="$HOME/.local/bin:$PATH" $HOME/dev/dotfiles/bin/lunar-max-brightness.sh
